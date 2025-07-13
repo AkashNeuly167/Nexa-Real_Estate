5ea546a157a88c0d61c0e8a7d40f3c3ab411e5d6
AkashNeuly <akashneuly123@gmail.com>
2025-07-13 12:59:58 +0530

    Fix getUser route or response

--- Client/src/Components/Contact.jsx
+++ Client/src/Components/Contact.jsx
@@ -11,7 +11,8 @@ export default function Contact({listing}) {
     useEffect(() => {
        const fetchLandLord = async ()=>{
         try {
-            const res  = await fetch (`${import.meta.env.VITE_API_BASE_URL}/api/user/${listing.userRef}`);
+            const res  = await fetch (`${import.meta.env.VITE_API_BASE_URL}/api/user/${listing.userRef},
+              credentials: 'include'`);
             const data = await res.json();
             setLandlord(data);
         } 
